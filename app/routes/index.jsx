070072174f09a78666e49f2275376292aadc438c
Alton <alton_ong@live.com.sg>
2023-03-29 17:57:37 +1100

    Initial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,14 @@
+export const meta = () => {
+  return {
+    title: 'Hydrogen',
+    description: 'A custom storefront powered by Hydrogen',
+  };
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h3>hello from Jleong!</h3>
+    </div>
+  );
+}
